7f552b131b17dc5986c5fb9464b0ea7400f01b6d
Akshata PM <94920947+Akshatapm@users.noreply.github.com>
2022-06-18 01:02:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show__menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show__menu");
+  });
+}
+
+// remove menu mobile
+
+const navLink = document.querySelectorAll(".nav__link");
+const linkaction = function () {
+  navMenu.classList.remove("show__menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkaction));
